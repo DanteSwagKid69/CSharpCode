49fe2c7c9b6cde239724d00aed080f7d8e2854b4
Dante von Matern <112094212+DanteSwagKid69@users.noreply.github.com>
2022-11-24 15:39:31 +0100

    Add files via upload

---
+++ ScoreBoard/Program.cs
@@ -0,0 +1,53 @@
+﻿
+Console.Clear();
+
+HighScoreHandler(Console.ReadLine());
+
+static void HighScoreHandler(string inputString)
+{
+    //Varibles
+    string fileName = "SavedScores.txt";
+
+    //debugg
+    if (inputString == "debug info")
+    {
+        Console.WriteLine(HighScore.Info());
+        Environment.Exit(0);
+    }
+
+    if (int.Parse(inputString) > int.Parse(HighScore.score))
+    {
+        HighScore.AddScore(int.Parse(inputString));
+        Console.WriteLine("NEW HIGHSCORE!");
+        Console.WriteLine("Write your name: ");
+        HighScore.AddName(Console.ReadLine());
+    }
+    else
+    {
+        Console.WriteLine("The number is smaller than " + HighScore.score);
+    }
+}
+
+
+public class HighScore
+{
+    public static string name = File.ReadAllText("SavedNames.txt");
+    public static string score = File.ReadAllText("SavedScores.txt");
+
+    public static void AddScore(int newScore)
+    {
+        File.WriteAllText("SavedScores.txt", newScore.ToString());
+    }
+
+    public static void AddName(string newName)
+    {
+        File.WriteAllText("SavedNames.txt", newName);
+    }
+
+    public static string Info()
+    {
+        return "Highscore holder is " + HighScore.name + " With the score " + HighScore.score;
+    }
+}
+
+
